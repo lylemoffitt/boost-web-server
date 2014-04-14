478282b1ebd96b9260ad2b97e7cc2d2babbd2722
TaniaNsouli <tinsouli@syr.edu>
2014-04-14 14:57:04 -0400

    Hi!

--- WebSystemsFinal/WebSystemsFinal.cpp
+++ WebSystemsFinal/WebSystemsFinal.cpp
@@ -1,11 +1,13 @@
 // WebSystemsFinal.cpp : Defines the entry point for the console application.
 //
-
+#include <iostream>
 #include "stdafx.h"
 
+using namespace std;
 
-int _tmain(int argc, _TCHAR* argv[])
+int main(int argc, char* argv[])
 {
+	cout << "Hi Lyle" << endl;  
 	return 0;
 }
 
