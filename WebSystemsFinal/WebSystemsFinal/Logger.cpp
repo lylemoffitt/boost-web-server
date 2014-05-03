031476918df307cf7c29eea6fb4de5dd769764fd
TaniaNsouli <tinsouli@syr.edu>
2014-05-03 02:16:29 -0400

    Update Logger.cpp

--- WebSystemsFinal/WebSystemsFinal/Logger.cpp
+++ WebSystemsFinal/WebSystemsFinal/Logger.cpp
@@ -7,3 +7,40 @@
 //
 
 #include "Logger.h"
+#define SIZE 1000
+using namespace std;
+
+void logger::log(std::string whom, int level, std::string info)
+{
+	stringstream ss;
+	ss << level;
+	string Level(ss.str());
+	int i = 0;
+	ifstream infile;
+	infile.open(Level);
+
+	string doc[SIZE];
+
+	while(!infile.eof)
+	{
+		infile >> doc[i];
+		i++;
+	}
+
+	infile.close();
+
+	ofstream outfile;
+
+	outfile.open(Level);
+	
+	while(i>-1)
+	{
+		outfile << doc[i];	
+	}
+	outfile << whom + info;
+
+	outfile.close();
+	
+
+
+}
