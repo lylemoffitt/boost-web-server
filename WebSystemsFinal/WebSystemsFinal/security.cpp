b4268299cde264ef5b4a83f4c15643e930e8d81b
OpenShift guest <tinsouli@syr.edu>
2014-05-03 14:31:27 -0400

    added stuff to security.cpp

--- WebSystemsFinal/WebSystemsFinal/security.cpp
+++ WebSystemsFinal/WebSystemsFinal/security.cpp
@@ -7,3 +7,30 @@
 //
 
 #include "security.h"
+
+using namespace std;
+
+bool security::chk_id(string usrnm, string pswd)
+{
+    int i = 0;
+    for (const string& x: user_set) 
+    {
+        if(user_set.id_table[i].username == usrnm)
+            if(user_set.id_table[i].pswd_hash == str_hasher(pswd))
+                return true;
+        i++;
+    }
+    return false;
+}
+    
+
+user security::mk_user(string usrnm, string pswd)
+{
+    user obj = new user(usrnam, pswd);
+    user_set.add_user(&obj);
+}
+    
+void security::report_fake()
+{
+    
+}
